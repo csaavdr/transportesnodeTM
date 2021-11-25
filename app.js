10a0464e0dedc50daa3b9a7cffceed5cd4f6a11d
Cristian Saavedra <crisjavisaa@hotmail.com>
2021-11-24 21:47:03 -0300

    migración del sitio, controladores, imagenes, css

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros'); //nosotros.js
+var serviciosRouter = require('./routes/servicios'); //servicios.js
+var galeriaRouter = require('./routes/galeria');
+var novedadesRouter = require('./routes/novedades');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -19,23 +23,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get("/nosotros", function(req, res, next) {
+//     res.send("Hola Curso!")
+// })
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/contacto', contactoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
