e791bc6d2030ef2e4ac68a83b01fdecf32bf0c1c
Cristian Saavedra <crisjavisaa@hotmail.com>
2021-12-10 17:35:33 -0300

    agregar novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -13,4 +13,38 @@ router.get('/', async function(req, res, next) {
     });
 });
 
+// vista del formulario de agregar
+router.get('/agregar', function(req, res, next) {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+
+    });
+});
+
+// procesa o da funcionamiento al boton guardar
+
+router.post('/agregar', async(req, res, next) => {
+        try {
+            console.log(req.body);
+
+            if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+                await novedadesModel.insertNovedad(req.body);
+                res.redirect('/admin/novedades');
+            } else {
+                res.render('admin/agregar', {
+                    layout: 'admin/layout',
+                    error: true,
+                    message: 'Todos los campos son requeridos'
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            res.render('admin/agregar', {
+                    layout: 'admin/layout',
+                    error: true,
+                    message: 'No se cargo la novedad'
+                }) //propiedades
+        } //cierre catch
+    }) // cierro router.post
+
 module.exports = router;
