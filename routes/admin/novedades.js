78196ca8b6a74cf2afa9de0e1568148a70bf0515
Cristian Saavedra <crisjavisaa@hotmail.com>
2021-12-16 00:55:49 -0300

    agregar, listar imagenes y tamb listar front

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModels');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
 
+//listado de novedades
 router.get('/', async function(req, res, next) {
 
     var novedades = await novedadesModel.getNovedades(); //query
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 100,
+                height: 100,
+                crop: 'fill',
+                radius: 'max'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -25,10 +49,20 @@ router.get('/agregar', function(req, res, next) {
 
 router.post('/agregar', async function(req, res, next) {
         try {
-            console.log(req.body);
+            // console.log(req.body);
+            var img_id = '';
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+            }
 
             if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-                await novedadesModel.insertNovedad(req.body);
+                // await novedadesModel.insertNovedad(req.body);
+                await novedadesModel.insertNovedad({
+                    ...req.body,
+                    img_id
+                })
+
                 res.redirect('/admin/novedades');
             } else {
                 res.render('admin/agregar', {
